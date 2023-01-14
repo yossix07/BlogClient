9da5f7ca9cc4ea3e324ba82e33479ec53e2a8fb2
Daniel Houri <danielsaadia.houri@live.biu.ac.il>
2023-01-15 00:23:41 +0200

    Fix

--- src/UsersPage/UsersPage.js
+++ src/UsersPage/UsersPage.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect, useNavigate } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { getUsersWithMoreThanAvgCommentsNum } from "../DB"
 import { Button, Card } from "react-bootstrap";
 import "./UsersPage.css";
 
 const UsersPage = () => {
     const navigate = useNavigate();
-    const [users, setUsers ] = useState([]);
+    const [users, setUsers] = useState([]);
 
     // fetch users
     useEffect(() => {
@@ -25,17 +26,17 @@ const UsersPage = () => {
             <Card>
                 <Card.Title className="bold">
                     Users With More Than Average Number Of Comments
-                    <Button onClick={ handleExit } variant="danger">X</Button>
+                    <Button onClick={handleExit} variant="danger">X</Button>
                 </Card.Title>
                 <Card.Body>
-                {users && typeof users === 'object' &&
-                    users.map((user, index) => 
-                    <Card key={index} className="user-item">
-                        <Card.Body> {user.userName} </Card.Body>
-                    </Card>
-                )}
+                    {users && typeof users === 'object' &&
+                        users.map((user, index) =>
+                            <Card key={index} className="user-item">
+                                <Card.Body> {user.userName} </Card.Body>
+                            </Card>
+                        )}
                 </Card.Body>
-                
+
             </Card>
         </div>
     )
